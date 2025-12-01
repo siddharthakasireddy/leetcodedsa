099e6f24ab9bd38600ce425007ed2afa9ca40008
siddharthakasireddy <siddharthakasireddy8@gmail.com>
2025-12-01 17:38:38 +0530

    Time: 0 ms (100%), Space: 12.8 MB (98.11%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}  # maps value -> index
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
+
+
+
+
+        
+        
