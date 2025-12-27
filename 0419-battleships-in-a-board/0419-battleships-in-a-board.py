3f961801be300b74fdb5e4cadffb1f84903e16ae
siddharthakasireddy <siddharthakasireddy8@gmail.com>
2025-12-27 23:27:31 +0530

    Time: 0 ms (100%), Space: 14.3 MB (53.02%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def countBattleships(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: int
+        """
+        if not board or not board[0]:
+            return 0
+        
+        m, n = len(board), len(board[0])
+        count = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'X':
+                    # Check if this is the start of a battleship
+                    if i > 0 and board[i-1][j] == 'X':
+                        continue
+                    if j > 0 and board[i][j-1] == 'X':
+                        continue
+                    count += 1
+        
+        return count
+
+        
